f7f57daf73e589a978c2f3979c4e41ad5ed67137
Maria Eduarda <145166801+maducsilva@users.noreply.github.com>
2025-04-03 00:47:38 -0300

    add exit button

--- Gestor/src/componentes/Profile/index.js
+++ Gestor/src/componentes/Profile/index.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import Styles from "./styles.js";
 
-export default function Profile({exit}) {
-    return (
-      <View style={Styles.container} >
-        <View style={Styles.profileBody}>
-          <View style={Styles.profile}>
-            <Image style={Styles.imageProfile} source={require('../../images/occurrenceImage.png')}/>
-            <Text style={Styles.textProfile}>Felipe João</Text>
-          </View>
-          {exit}
+export default function Profile({ exit, navigation,page }) {
+  return (
+    <View style={Styles.container}>
+      <View style={Styles.profileBody}>
+        <View style={Styles.profile}>
+          <Image
+            style={Styles.imageProfile}
+            source={require("../../images/occurrenceImage.png")}
+          />
+          <Text style={Styles.textProfile}>Felipe João</Text>
         </View>
+        {exit && (
+          <TouchableOpacity onPress={() => navigation.navigate(page)}>
+            {exit}
+          </TouchableOpacity>
+        )}
       </View>
-    );
-  }
+    </View>
+  );
+}
--- Gestor/src/pages/Main/index.js
+++ Gestor/src/pages/Main/index.js
@@ -3,9 +3,10 @@ import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import Styles from "./styles";
 import globalStyles from "../../Styles/globalStyles";
 import Card from "../../componentes/Card/index";
-import Profile from "../../componentes/Profile"
-import GraphComponent from "../../componentes/Graphic";  // Importa o componente de gráfico
+import Profile from "../../componentes/Profile";
+import GraphComponent from "../../componentes/Graphic"; // Importa o componente de gráfico
 import { fetchOccurrencesData } from "../../API/api";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
 
 export default function Main({ navigation }) {
   const [occurrences, setOccurrences] = useState([]);
@@ -25,17 +26,17 @@ export default function Main({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
-      <Profile />
+      <Profile page='Login' navigation={navigation} exit={<FontAwesome6 name="arrow-right-from-bracket" size={32} />} />
       <View style={Styles.horizontalLine} />
       <View style={Styles.body}>
         <View style={{ alignItems: "start", width: "90%", margin: "2%" }}>
           <Text style={Styles.title}> Últimas Ocorrências:</Text>
         </View>
-        
+
         {/* <View style={Styles.containerInfo}></View> */}
-        
+
         <GraphComponent />
       </View>
-    </View> 
+    </View>
   );
 }

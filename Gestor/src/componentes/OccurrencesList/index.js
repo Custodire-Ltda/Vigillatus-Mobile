301a08a4a38833ac84117bc63fb06d6dc75b7e2e
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2025-05-26 19:45:09 -0300

    Fetch occurences at parent component Ocorrencias/index.js

--- Gestor/src/componentes/OccurrencesList/index.js
+++ Gestor/src/componentes/OccurrencesList/index.js
@@ -3,59 +3,28 @@ import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Styles from "./styles.js";
 import OcorrencesItem from "../../componentes/OccurrencesList/OccurrencesItem/index.js";
-import Gestor from "../../API/Gestor.js";
-import { api } from "../../API/api.js";
 
 export default function OccurrencesList({ data, title }) {
-  const [loggedGestor, setLoggedGestor] = useState({});
-  const [occurrencesList, setOccurrencesList] = useState([]);
-
   const navigation = useNavigation();
-
-  const gestorService = new Gestor();
-
-  useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const gestorInfo = await gestorService.getLoggedGestor();
-          const gestorToken = await gestorService.getToken();
-
-          setLoggedGestor(gestorInfo);
-
-          const occurrencesList = await api.get("Ocorrencia/GestorOcorrencias/" + loggedGestor._id, {
-            headers: {
-              authorization: `Bearer ${gestorToken}`
-            }
-          });
-
-          setOccurrencesList(occurrencesList.data)
-
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    
-      fetchData();
-  }, []);
 
   return (
     <View style={Styles.container}>
       <View style={Styles.subNavbar}>
         <Text style={Styles.textSubNavbar}>{title}</Text>
       </View>
-        <FlatList
+      <FlatList
         style={Styles.body}
-          data={occurrencesList}
-          keyExtractor={(item) => item._id.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("Ocorrencia", { item })}
-            >
+        data={data}
+        keyExtractor={(item) => item._id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Ocorrencia", { item })}
+          >
             <OcorrencesItem {...item} />
-            </TouchableOpacity>
-          )}
-          ItemSeparatorComponent={() => <View style={Styles.horizontalLine} />}
-        />
+          </TouchableOpacity>
+        )}
+        ItemSeparatorComponent={() => <View style={Styles.horizontalLine} />}
+      />
     </View>
   );
 }
--- Gestor/src/pages/Ocorrencias/index.js
+++ Gestor/src/pages/Ocorrencias/index.js
@@ -4,24 +4,54 @@ import globalStyles from "../../Styles/globalStyles";
 import styles from "./styles.js";
 import Search from "../../componentes/ExpansiveSearch";
 import OccurrencesList from "../../componentes/OccurrencesList/index.js";
+import Gestor from "../../API/Gestor.js";
+import { api } from "../../API/api.js";
 
 export default function Ocorrencias() {
   const [occurrences, setOccurrences] = useState([]);
   const [filteredOccurrences, setFilteredOccurrences] = useState([]);
+  const [occurrencesList, setOccurrencesList] = useState([]);
+  const [loggedGestor, setLoggedGestor] = useState({});
+
+  const gestorService = new Gestor();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const gestorInfo = await gestorService.getLoggedGestor();
+        const gestorToken = await gestorService.getToken();
+
+        setLoggedGestor(gestorInfo);
+
+        const occurrencesList = await api.get("Ocorrencia/GestorOcorrencias/" + loggedGestor._id, {
+          headers: {
+            authorization: `Bearer ${gestorToken}`
+          }
+        });
+
+        setOccurrencesList(occurrencesList.data)
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <View style={globalStyles.container}>
       <View style={styles.container}>
         <View style={styles.containerSearch}>
-          <Search
+          {/* <Search
             results={occurrences}
-            setFilteredResults={setFilteredOccurrences}
-          />
+            setFilteredResults={occurrencesList}
+          /> */}
         </View>
       </View>
       <OccurrencesList
         title={<Text>Ocorrências</Text>}
-        data={filteredOccurrences}
+        data={occurrencesList}
       />
     </View>
   );
